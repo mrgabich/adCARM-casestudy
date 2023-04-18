ba0658ce936c2b7acf95ed40998bc99885046713
Geancarlo Abich <geancarlo.abich@tkelevator.com>
2023-04-18 11:26:01 -0300

    FIX: roofline graph

--- run.py
+++ run.py
@@ -50,10 +50,9 @@ def carm_eq(ai, bw, fp):
 def plot_roofline(name, data, ct):
 
     fig, ax = plt.subplots(figsize=(7.1875*1.5,3.75*1.5))
-    plt.xlim(0.015625, 256)
-    plt.ylim(0.25, round_power_of_2(int(data['FP'])))
     ai = np.linspace(0.00390625, 256, num=200000)
-
+    plt.xlim(0.00390625, 256)
+    plt.ylim(carm_eq(ai, data['DRAM'], data['FP']), round_power_of_2(int(data['FP']*1.1)))
     #Ploting Lines
     plt.plot(ai, carm_eq(ai, data['L1'], data['FP']), 'k', lw = 3, label='L1')
     plt.plot(ai, carm_eq(ai, data['L2'], data['FP']), 'grey', lw = 3, label='L2')
